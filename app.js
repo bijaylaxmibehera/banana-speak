31cc4d1d3375ac4adfaa38f0939925fe92f683fc
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2022-09-02 22:55:47 +0530

    input, output and processing done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
-console.log(txtInput);
+//console.log(txtInput);
+var outputDiv= document.querySelector("#output");
+//console.log(outputDiv);
 //callback
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input",txtInput.value);
+   outputDiv.innerText= "abnmcjk "+txtInput.value;
 }
 btnTranslate.addEventListener("click",clickHandler);
 
