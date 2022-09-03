227523be6d1434051e210910b688ef35857b7dea
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2022-09-03 21:02:32 +0530

    done

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
+var txtInput = document.querySelector("#txt-input");
 //console.log(txtInput);
-var outputDiv= document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
 //console.log(outputDiv);
-//callback
-function clickHandler() {
-   outputDiv.innerText= "abnmcjk "+txtInput.value;
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+   return serverURL + "?" + "text=" + text;
 }
+function errorHandler(error) {
+   console.log("error occured", error);
+   alert("Something wrong with server! try again after sometime");
+}
+function clickHandler() {
+   //taking input
+   var inputText = txtInput.value;
+   //calling server for processing
+   fetch(getTranslationURL(inputText))
+      .then(response => response.json())
+      .then(json => {
+         var translatedText = json.contents.translated;
+         outputDiv.innerText = translatedText; // output
+        })
+      .catch(errorHandler)
+}
+
 btnTranslate.addEventListener("click",clickHandler);
+
+//callback
+// function clickHandler() {
+//    outputDiv.innerText= "abnmcjk "+txtInput.value;
+// }
+
 
 // console.log(btnTranslate);
